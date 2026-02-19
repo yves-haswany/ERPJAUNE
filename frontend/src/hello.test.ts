1af0fca2b2c8ab119715f21e729645a5477b3ca8
= <=>
2026-02-19 11:03:17 +0200

    Add tailwind

--- frontend/src/hello.test.ts
+++
@@ -1,5 +0,0 @@
-const { test, expect } = require('vitest');
-
-test('hello world!', () => {
-	expect(1 + 1).toBe(2);
-});
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { loginUser } from "../services/api";
+import { loginUser } from "@/services/api";
 
 export default function Login() {
   const [email, setEmail] = useState("");
