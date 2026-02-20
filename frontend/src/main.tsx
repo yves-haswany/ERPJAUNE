458bab5c586fe5596ab17dd46712945e22eb3067
= <=>
2026-02-20 10:24:29 +0200

    Fix main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <AuthProvider>
       <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
