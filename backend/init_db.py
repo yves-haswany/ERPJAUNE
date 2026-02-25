97ad6a69e2f3577639e67e62735a12d4e5419ec2
= <=>
2026-02-25 12:46:39 +0200

    Initializedatabase

---
+++ backend/init_db.py
@@ -0,0 +1,13 @@
+import asyncio
+from app.core.database import engine, Base
+
+# IMPORTANT: import ALL models here
+from app.models.user import User
+from app.models.tenant import Tenant
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+asyncio.run(init_db())
